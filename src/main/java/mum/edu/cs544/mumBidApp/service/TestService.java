f5c62887a8d9f7da7347d47ec4ea9c53e2324d20
geetaputtappanavar <geeta.271@gmail.com>
2017-12-19 15:06:46 -0600

    by Geeta

--- src/main/java/mum/edu/cs544/mumBidApp/service/TestService.java
+++ src/main/java/mum/edu/cs544/mumBidApp/service/TestService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import mum.edu.cs544.mumBidApp.DAO.TestDao;
-import mum.edu.cs544.mumBidApp.model.User;
+
 
 @Service
 public class TestService {
